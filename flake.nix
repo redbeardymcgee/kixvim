35c5fa346088661a17ab85f8d46710727f71847b
redbeardymcgee <redbeardymcgee@gmail.com>
2024-07-11 21:44:56 -0500

    init: first test commit

--- flake.nix
+++ flake.nix
@@ -8,10 +8,10 @@
 
     # Add bleeding-edge plugins here.
     # They can be updated with `nix flake update` (make sure to commit the generated flake.lock)
-    # wf-nvim = {
-    #   url = "github:Cassin01/wf.nvim";
-    #   flake = false;
-    # };
+    wf-nvim = {
+      url = "github:Cassin01/wf.nvim";
+      flake = false;
+    };
   };
 
   outputs = inputs @ {
@@ -35,7 +35,7 @@
       pkgs = import nixpkgs {
         inherit system;
         overlays = [
-          # Import the overlay, so that the final Neovim derivation(s) can be accessed via pkgs.<nvim-pkg>
+          # Import the overlay, so that the final Neovim derivation(s) can be accessed via pkgs.<kixvim>
           neovim-overlay
           # This adds a function can be used to generate a .luarc.json
           # containing the Neovim API all plugins in the workspace directory.
@@ -60,7 +60,7 @@
     in {
       packages = rec {
         default = nvim;
-        nvim = pkgs.nvim-pkg;
+        nvim = pkgs.kixvim;
       };
       devShells = {
         default = shell;
